de23b0a3b43c4768ee1abf3f3fe880116d358b05
hai <thaiviethai99@gmail.com>
2020-09-06 10:52:45 +0700

    first code

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Page, Navbar, Link, Block } from 'framework7-react';
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Page>
+        <Navbar title="Home Page" />
+        <Block>
+          <Link href="/about/">About Page</Link>
+        </Block>
+      </Page>
+    )
+  }
+
+}
+
+export default Home;
+
